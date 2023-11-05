c8263ab9c19bd82d5656b9ae58679fa6c0b46714
Gvynhartt <savshush134@gmail.com>
2023-11-06 01:24:15 +0300

    empty line and other activity tests added for task #1

---
+++ app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -0,0 +1,147 @@
+package ru.netology.testing.uiautomator
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+const val SETTINGS_PACKAGE = "com.android.settings"
+const val MODEL_PACKAGE = "ru.netology.testing.uiautomator"
+
+const val TIMEOUT = 5000L
+
+@RunWith(AndroidJUnit4::class)
+class ChangeTextTest {
+
+    private lateinit var device: UiDevice
+    private val textToSet = "Netology"
+    private val emptyLineToSet = ""
+    private val nonEmptyLineExternal = "Deep, dark fantasies..."
+
+//    @Test
+//    fun testInternetSettings() {
+//    // Press home
+//    device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+//    device.pressHome()
+//
+//    // Wait for launcher
+//    val launcherPackage = device.launcherPackageName
+//    device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
+//    // waitForPackage(SETTINGS_PACKAGE)
+//
+//    val context = ApplicationProvider.getApplicationContext<Context>()
+//    val intent = context.packageManager.getLaunchIntentForPackage(SETTINGS_PACKAGE)
+//    context.startActivity(intent)
+//    device.wait(Until.hasObject(By.pkg(SETTINGS_PACKAGE)), TIMEOUT)
+//
+//    device.findObject(
+//    UiSelector().resourceId("android:id/title").instance(0)
+//    ).click()
+//    }
+//    @Test
+//    fun testChangeText() {
+////     Press home
+//    device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+//    device.pressHome()
+//
+//    // Wait for launcher
+//    val launcherPackage = device.launcherPackageName
+//    device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
+//    // waitForPackage(SETTINGS_PACKAGE)
+//
+//    val context = ApplicationProvider.getApplicationContext<Context>()
+//    // val packageName = context.packageName
+//    val intent = context.packageManager.getLaunchIntentForPackage(MODEL_PACKAGE)
+//    context.startActivity(intent)
+//    device.wait(Until.hasObject(By.pkg(MODEL_PACKAGE)), TIMEOUT)
+//
+//
+//    device.findObject(By.res(MODEL_PACKAGE, "userInput")).text = textToSet
+//    device.findObject(By.res(MODEL_PACKAGE, "buttonChange")).click()
+//
+//    val result = device.findObject(By.res(MODEL_PACKAGE, "textToBeChanged")).text
+//    assertEquals(textToSet, result)
+//    }
+//
+    private fun waitForPackage(packageName: String) {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+        context.startActivity(intent)
+        device.wait(Until.hasObject(By.pkg(packageName)), TIMEOUT)
+    }
+
+    @Before
+    fun beforeEachTest() {
+        // Press home
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.pressHome()
+
+        // Wait for launcher
+        val launcherPackage = device.launcherPackageName
+        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
+    }
+
+    @Test
+    fun testInternetSettings() {
+        waitForPackage(SETTINGS_PACKAGE)
+
+        device.findObject(
+            UiSelector().resourceId("android:id/title").instance(0)
+        ).click()
+    }
+
+    @Test
+    fun testChangeText() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(packageName, "userInput")).text = textToSet
+        device.findObject(By.res(packageName, "buttonChange")).click()
+
+        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+        assertEquals(textToSet, result)
+
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun testInputEmptyLine() {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        val expected = device.findObject(By.res(MODEL_PACKAGE, "textToBeChanged")).text
+        // для упрощения проверки получаем сперва дефолтный текст из элемента
+        device.findObject(By.res(MODEL_PACKAGE, "userInput")).text = emptyLineToSet
+        device.findObject(By.res(MODEL_PACKAGE, "buttonChange")).click()
+
+        val result = device.findObject(By.res(MODEL_PACKAGE, "textToBeChanged")).text
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testOpenInputTextInAnotherActivity() {
+
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        device.findObject(By.res(MODEL_PACKAGE, "userInput")).text = nonEmptyLineExternal
+        device.findObject(By.res(MODEL_PACKAGE, "buttonActivity")).click()
+        // наконец-то происходит что-то интересное
+        device.wait(Until.hasObject(By.res(MODEL_PACKAGE, "text")), TIMEOUT)
+        //  дожидаемся появления нужного элемента с нашим текстом на втором экране
+        val result = device.findObject(By.res(MODEL_PACKAGE, "text")).text
+        assertEquals(nonEmptyLineExternal, result)
+    }
+}
+
+
+
